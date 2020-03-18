0b193bff348620e18d57e771fb59e90d82fa326c
Patricia <patricia.mesesan.pm@gmail.com>
2020-03-18 09:03:06 +0200

    Teste

--- src/test/java/LabAssiAsseProjectV04Maven/AppTest.java
+++ src/test/java/LabAssiAsseProjectV04Maven/AppTest.java
@@ -1,7 +1,13 @@
 package LabAssiAsseProjectV04Maven;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import LabAssiAsseProjectV04Maven.src.Domain.Student;
+import LabAssiAsseProjectV04Maven.src.Repository.StudentRepo;
+import LabAssiAsseProjectV04Maven.src.Service.ServiceStudent;
+import LabAssiAsseProjectV04Maven.src.Validator.StudentValidator;
+import LabAssiAsseProjectV04Maven.src.Validator.Validator;
 import org.junit.Test;
 
 /**
@@ -17,5 +23,29 @@ public class AppTest
     {
 
         assertTrue( true );
+    }
+
+    @Test
+    public void addStudent1()
+    {
+        Student st = new Student("100", "Alex", 934, "alex@email.com", "Professor Professor");
+        Validator<Student> sv= new StudentValidator();
+        StudentRepo repo = new StudentRepo(sv, "studenti.txt");
+        ServiceStudent serviceSt = new ServiceStudent(repo);
+        serviceSt.add(st);
+        Student s = serviceSt.find("100");
+        assertEquals("Alex", s.getNume());
+    }
+
+    @Test
+    public void addStudent2()
+    {
+        Student st = new Student("101", "Alex1", 934, "alex", "Professor professor");
+        Validator<Student> sv= new StudentValidator();
+        StudentRepo repo = new StudentRepo(sv, "studenti.txt");
+        ServiceStudent serviceSt = new ServiceStudent(repo);
+        serviceSt.add(st);
+        Student s = serviceSt.find("101");
+        assertEquals("Alex1", s.getNume());
     }
 }
