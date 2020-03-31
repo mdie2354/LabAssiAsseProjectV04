f8c60c4cb8444a6e29aedfb430976e1ea61dc86b
Patricia <patricia.mesesan.pm@gmail.com>
2020-03-31 14:10:47 +0300

    Lab3TestCases

--- src/test/java/LabAssiAsseProjectV04Maven/AppTest.java
+++ src/test/java/LabAssiAsseProjectV04Maven/AppTest.java
@@ -4,17 +4,59 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import LabAssiAsseProjectV04Maven.src.Domain.Student;
+import LabAssiAsseProjectV04Maven.src.Domain.Teme;
 import LabAssiAsseProjectV04Maven.src.Repository.StudentRepo;
+import LabAssiAsseProjectV04Maven.src.Repository.TemeRepo;
 import LabAssiAsseProjectV04Maven.src.Service.ServiceStudent;
+import LabAssiAsseProjectV04Maven.src.Service.ServiceTeme;
 import LabAssiAsseProjectV04Maven.src.Validator.StudentValidator;
+import LabAssiAsseProjectV04Maven.src.Validator.TemeValidator;
+import LabAssiAsseProjectV04Maven.src.Validator.ValidationException;
 import LabAssiAsseProjectV04Maven.src.Validator.Validator;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
+
+    /* Lab 3 - In class*/
+
+    private TemeRepo temeRepo;
+    private ServiceTeme serviceTeme;
+
+    @Before
+    public void setUp() {
+        temeRepo = new TemeRepo(new TemeValidator(), "teme.txt");
+        serviceTeme = new ServiceTeme(temeRepo);
+    }
+
+    @Test
+    public void test_invalidAssignment() {
+        Teme tema1 = new Teme(-1, "", -1, -2);
+        try {
+            serviceTeme.add(tema1);
+        } catch (ValidationException validationException) {
+            assertThat(validationException.getMessage(), is("\nID invalid\nDeadline invalid\nSaptamana in care tema a fost primita este invalida"));
+        }
+    }
+
+    @Test
+    public void test_validAssignment() {
+        Teme tema1 = new Teme(1, "Tema1", 1, 2);
+        try {
+            serviceTeme.add(tema1);
+        } catch (ValidationException validationException) {
+            assertThat(validationException.getMessage(), is("\nID invalid\nDeadline invalid\nSaptamana in care tema a fost primita este invalida"));
+        }
+    }
+
+
     /**
      * Rigorous Test :-)
      */
@@ -53,4 +95,7 @@ public class AppTest
             assertEquals("\nEmail invalid", ex.getMessage());
         }
     }
+
+
+
 }
