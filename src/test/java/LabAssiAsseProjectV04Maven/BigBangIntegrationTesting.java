92cc5dbb5b1c7cfcadbf677504bdeb5326bf4c9a
medreadiana <diana.medrea98@gmail.com>
2020-04-14 13:32:00 +0300

    added big bang integration testing class

---
+++ src/test/java/LabAssiAsseProjectV04Maven/BigBangIntegrationTesting.java
@@ -0,0 +1,19 @@
+package LabAssiAsseProjectV04Maven;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BigBangIntegrationTesting {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        assertTrue( true );
+    }
+
+}
